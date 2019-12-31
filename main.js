3f707109cc8f5126548f0bbf4bdec49da3c2e1ce
JulesDemoulin <jdemoulin51@gmail.com>
2019-12-31 23:16:43 +0100

    Changement TOKEN + ajout Prefixe

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Client } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
 const client = new Client({ disableEveryone: true });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Je suis prêt !`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send('@everyone, salut à tous !', { disableEveryone: false });
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), salut à tous !');
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send('Pong!');
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send('Ping!');
 });
 
-client.login('NjYxNTc4NjMyMzA0NjU2NDE3.Xguc2w.TDh0cDh4by9FYiggwIaNkc1uaTY');
+client.login(TOKEN);
